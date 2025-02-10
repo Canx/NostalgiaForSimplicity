5c4c42e658f8f7d131aa7cb480c43b7d39da41b2
Ruben Cancho <304053+Canx@users.noreply.github.com>
2025-02-10 15:40:02 +0100

    Testing hyperopt from signals

--- user_data/strategies/signals/Entry_SMAOffset.py
+++ user_data/strategies/signals/Entry_SMAOffset.py
@@ -1,17 +1,23 @@
 from Signal import Signal
 from pandas import DataFrame
 import pandas as pd
-import talib as ta
+from freqtrade.strategy import IntParameter
+from freqtrade.strategy import IStrategy
 
 
 class Entry_SMAOffset(Signal):
+
     def __init__(self, priority: int = 105):
-        super().__init__(priority, enabled=False)
-    
+        super().__init__(priority, enabled=True)
+
+    def config_strategy(self, strat: IStrategy):
+        strat.buy_rsi = IntParameter(50, 70, default=59, space="buy")
+        super().config_strategy(strat)
+  
 
     def entry_signal(self, df: DataFrame, metadata: dict) -> pd.Series:
         return (
             (df['close'] < df['EMA_16'] * 0.973) &
             (df['EWO'] > 5.672) &
-            (df['RSI_14'] < 59)
+            (df['RSI_14'] < self.strat.buy_rsi.value)
         )

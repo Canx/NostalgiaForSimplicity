c3c460e63f403d053acd88075198c59f044be3a8
Rubén Cancho <304053+Canx@users.noreply.github.com>
2025-02-11 15:10:37 +0100

    Fixed kamafama2

--- user_data/strategies/signals/Entry_KamaFama2.py
+++ user_data/strategies/signals/Entry_KamaFama2.py
@@ -7,7 +7,7 @@ import talib as ta
 class Entry_KamaFama2(Signal):
     def init(self):
         self.priority = 105
-        self.enabled = False
+        self.enabled = True
     
     def plot_config(self):
         plot_config = {}
@@ -18,31 +18,34 @@ class Entry_KamaFama2(Signal):
             'mama_offset_0981': { 'color': 'orange'}
         }
         plot_config['subplots'] = {
-            "kama > fama": {
+            "entry_KF": {
+                'entry_KF': {'color': 'white'},
+            },
+            "kama_over_fama": {
                 'kama_over_fama': {'color': 'red'},
             },
-            "fama > mama*0.981": {
+            "fama_over_mama": {
                 'fama_over_mama': { 'color': 'blue'},
             },
-            "WILLR < -61.3": {
+            "WILLR_KF": {
                 'WILLR_KF': { 'color': 'green'},
             },
-            "mama_diff < -0.025": {
+            "mamadiff_KF": {
                 "mamadiff_KF": { 'color': 'orange'}
             },
-            "cti < -0.715": {
+            "cti_KF": {
                 "cti_KF": { 'color': 'purple'}
             },
-            "close < close48 * 0.98": {
+            "close288_KF": {
                 "close48_KF": { 'color': 'yellow'}
             },
             "close < close288 * 0.96": {
                 "close288_KF": { 'color': 'white'}
             },
-            "RSI84 < 60": {
+            "RSI84_KF": {
                 "RSI84_KF": { 'color': 'maroon'}
             },
-            "RSI112 < 60": {
+            "RSI112_KF": {
                 "RSI112_KF": { 'color': 'pink'}
             }
         }
@@ -51,14 +54,4 @@ class Entry_KamaFama2(Signal):
 
 
     def entry_signal(self, df: DataFrame, metadata: dict) -> pd.Series:
-        return (
-            (df['kama_over_fama']) &
-            (df['fama_over_mama']) &
-            (df['WILLR_KF']) &
-            (df['mamadiff_KF']) &
-            (df['cti_KF']) &
-            (df['close48_KF']) &
-            (df['close288_KF']) &
-            (df['RSI84_KF']) &
-            (df['RSI112_KF'])
-        )
+        return (df['entry_KF'])
